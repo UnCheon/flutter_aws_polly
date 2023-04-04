54294d3d3b5d35c72490b7091ab1ce011defbcad
uncheon <ypunval@gmail.com>
2023-04-04 12:10:31 +0900

    Update AWSPollyVoiceId+Helpers.swift
    update voice ids

--- ios/Classes/AWSPollyVoiceId+Helpers.swift
+++ ios/Classes/AWSPollyVoiceId+Helpers.swift
@@ -11,8 +11,13 @@ extension AWSPollyVoiceId {
     static func voiceIdForString(voiceIdString: String) -> AWSPollyVoiceId {
         switch voiceIdString {
             case "aditi": return .aditi
+            case "adriano": return .adriano
             case "amy": return .amy
+            case "andres": return .andres
+            case "aria": return .aria
+            case "arthur": return .arthur
             case "astrid": return .astrid
+            case "ayanda": return .ayanda
             case "bianca": return .bianca
             case "brian": return .brian
             case "camila": return .camila
@@ -22,7 +27,10 @@ extension AWSPollyVoiceId {
             case "chantal": return .chantal
             case "conchita": return .conchita
             case "cristiano": return .cristiano
+            case "daniel": return .daniel
             case "dora": return .dora
+            case "elan": return .elan
+            case "elin": return .elin
             case "emma": return .emma
             case "enrique": return .enrique
             case "ewa": return .ewa
@@ -31,7 +39,12 @@ extension AWSPollyVoiceId {
             case "geraint": return .geraint
             case "giorgio": return .giorgio
             case "gwyneth": return .gwyneth
+            case "hala": return .hala
+            case "hannah": return .hannah
             case "hans": return .hans
+            case "hans": return .hans
+            case "hiujin": return .hiujin
+            case "ida": return .ida
             case "ines": return .ines
             case "ivy": return .ivy
             case "jacek": return .jacek
@@ -39,11 +52,15 @@ extension AWSPollyVoiceId {
             case "joanna": return .joanna
             case "joey": return .joey
             case "justin": return .justin
+            case "kajal": return .kajal
             case "karl": return .karl
+            case "kazuha": return .kazuha
             case "kendra": return .kendra
             case "kevin": return .kevin
             case "kimberly": return .kimberly
+            case "laura": return .laura
             case "lea": return .lea
+            case "liam": return .liam
             case "liv": return .liv
             case "lotte": return .lotte
             case "lucia": return .lucia
@@ -57,24 +74,31 @@ extension AWSPollyVoiceId {
             case "mia": return .mia
             case "miguel": return .miguel
             case "mizuki": return .mizuki
+            case "montserrat": return .montserrat
             case "naja": return .naja
             case "nicole": return .nicole
+            case "ola": return .ola
             case "olivia": return .olivia
+            case "pedro": return .pedro
             case "penelope": return .penelope
             case "raveena": return .raveena
+            case "remi": return .remi
             case "ricardo": return .ricardo
             case "ruben": return .ruben
             case "russell": return .russell
+            case "ruth": return .ruth
             case "salli": return .salli
             case "seoyeon": return .seoyeon
+            case "sergio": return .sergio
+            case "stephen": return .stephen
             case "takumi": return .takumi
             case "tatyana": return .tatyana
+            case "thiago": return .thiago
+            case "tomoko": return .tomoko
             case "vicki": return .vicki
             case "vitoria": return .vitoria
             case "zeina": return .zeina
             case "zhiyu": return .zhiyu
-            case "aria": return .aria
-            case "ayanda": return .ayanda
             default: return .unknown
         }
     }
