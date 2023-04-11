1c7912212a741c4544ac609b108d0afc6d05c16f
uncheon <ypunval@gmail.com>
2023-04-11 10:42:34 +0900

    Update SwiftAwsPollyPlugin.swift

--- ios/Classes/SwiftAwsPollyPlugin.swift
+++ ios/Classes/SwiftAwsPollyPlugin.swift
@@ -52,7 +52,8 @@ public class SwiftAwsPollyPlugin: NSObject, FlutterPlugin {
 
       // Text to synthesize
       input.text = text
-      input.textType = "ssml"
+      input.speechMarkTypes = [.sentence, .ssml, .viseme, .word]
+
 
       // We expect the output in MP3 format
       input.outputFormat = AWSPollyOutputFormat.mp3
